2f63f405b3aceddcb63c8e4e9a704f27006184a6
Julian L <julian.lawrance@gmail.com>
2024-05-07 19:16:54 +0100

    Start implementing enemy-bullet collision

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,8 +3,19 @@ extends PathFollow2D
 @export_range(0.1, 0.2)
 var movement_speed: float
 
+@onready var stats: EnemyStats = $Stats
+
+signal hit(body:Node2D)
+
 func _process(delta):
 	move(delta)
 
 func move(delta):
 	progress_ratio += movement_speed * delta
+
+func _on_collision_area_body_entered(body:Node2D):
+	# TODO: this is not firing when the bullet enters.
+	# Something wrong with the collision layers/masks?
+	hit.emit(body)
+	stats.take_damage(1)
+	body.queue_free()
--- scripts/enemy_stats.gd
+++ scripts/enemy_stats.gd
@@ -1,4 +1,13 @@
-extends Node
+class_name EnemyStats extends Node
 
 @export_range(1, 5)
 var starting_health: int
+
+var current_health: int
+
+func _ready():
+    current_health = starting_health
+
+func take_damage(amount: int):
+    current_health -= amount
+    print("Took " + str(amount) + " damage, health is now " + str(current_health))
