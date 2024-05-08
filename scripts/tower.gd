80b7ba4d6d543530ca437d743f24501de6646539
Julian Lawrance <julian.lawrance@gmail.com>
2024-05-08 12:43:12 +0100

    Allow deselecting a tower with Escape key

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -1,12 +1,33 @@
-extends Node2D
+class_name Tower extends Node2D
 
 @onready var range_node = $Range
 @onready var selection_node = $Selection
 
 var is_selected = false
 
+signal on_selected
+signal on_deselected
+
 func _ready():
+	deselect()
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		deselect()
+
+func select():
+	selection_node.show()
+	range_node.show()
+	is_selected = true
+
+	on_selected.emit()
+
+func deselect():
+	selection_node.hide()
 	range_node.hide()
+	is_selected = false
+
+	on_deselected.emit()
 
 func _on_collision_area_mouse_entered():
 	range_node.show()
@@ -17,5 +38,4 @@ func _on_collision_area_mouse_exited():
 
 func _on_collision_area_input_event(viewport:Node, event:InputEvent, shape_idx:int):
 	if event.is_pressed():
-		selection_node.show()
-		is_selected = true
+		select()
