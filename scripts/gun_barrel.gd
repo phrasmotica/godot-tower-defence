bc681d7060db05e1332001ee528ffb2927ad4ef1
Julian Lawrance <julian.lawrance@gmail.com>
2024-05-08 13:02:37 +0100

    Create gun barrel scene and add shot timer to it

--- scripts/gun_barrel.gd
+++ scripts/gun_barrel.gd
@@ -2,7 +2,6 @@ class_name GunBarrel extends Node2D
 
 @export var projectile: PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var bullet: Projectile = projectile.instantiate()
+func _on_shot_timer_timeout():
+	var bullet = projectile.instantiate()
 	add_child(bullet)
