8a51eaa1e94a95d01a3ae585105814d817786f66
Julian Lawrance <julian.lawrance@gmail.com>
2024-05-10 12:50:10 +0100

    Animate tower warmup with progress bar

--- scripts/tower_level_manager.gd
+++ scripts/tower_level_manager.gd
@@ -2,8 +2,12 @@ class_name TowerLevelManager extends Node2D
 
 @onready var firing_line: RayCast2D = $FiringLine
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var warmup_bar: ColorRect = $WarmupProgressBar
 
 signal warmed_up
+
+func _ready():
+	warmup_bar.hide()
 
 func start_warmup():
 	animation_player.play("warmup")
