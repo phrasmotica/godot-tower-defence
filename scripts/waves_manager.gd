91e09c3534a8540607255c2262d7d06560f5f1b4
Julian Lawrance <julian.lawrance@gmail.com>
2024-05-13 13:01:41 +0100

    Implement a basic wave sequence

--- scripts/waves_manager.gd
+++ scripts/waves_manager.gd
@@ -4,6 +4,15 @@ class_name WavesManager extends Node
 
 @export var enemy_scene: PackedScene
 
+var wave_number = 0
+
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
+		next()
+
+func next():
+	wave_number += 1
+
+	for i in range(wave_number):
 		path.spawn_enemy(enemy_scene)
+		await get_tree().create_timer(1.0).timeout
