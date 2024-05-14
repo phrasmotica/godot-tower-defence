e3522a6f147009fa65ef624c082d33ab4e67e9b3
Julian Lawrance <julian.lawrance@gmail.com>
2024-05-14 13:32:51 +0100

    Update lives label in game UI when lives changes

--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -1,6 +1,7 @@
 class_name GameUI extends Control
 
 @onready var money_amount = $ColorRect/MoneyLabel/Amount
+@onready var lives_amount = $ColorRect/LivesLabel/Amount
 
 signal buy_gun_tower_button
 
@@ -12,3 +13,7 @@ func _on_gun_tower_button_pressed():
 func _on_bank_manager_money_changed(new_money:int):
 	if money_amount:
 		money_amount.text = str(new_money)
+
+func _on_lives_manager_lives_changed(new_lives):
+	if lives_amount:
+		lives_amount.text = str(new_lives)
