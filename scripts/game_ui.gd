794bc62ee3cda070299b10ec5e45b79e562adf60
Julian Lawrance <julian.lawrance@gmail.com>
2024-05-14 13:30:10 +0100

    Update money label in game UI when money changes

--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -1,8 +1,14 @@
 class_name GameUI extends Control
 
+@onready var money_amount = $ColorRect/MoneyLabel/Amount
+
 signal buy_gun_tower_button
 
 func _on_gun_tower_button_pressed():
 	print("Buying gun tower from UI")
 
 	buy_gun_tower_button.emit()
+
+func _on_bank_manager_money_changed(new_money:int):
+	if money_amount:
+		money_amount.text = str(new_money)
