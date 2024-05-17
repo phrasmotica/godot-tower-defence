5468b6037707c42182cb74ada1169b6a187e9788
Julian Lawrance <julian.lawrance@gmail.com>
2024-05-17 08:53:33 +0100

    Properly instantiate slow effect scene when creating effect

--- scripts/effect_stats.gd
+++ scripts/effect_stats.gd
@@ -1,12 +1,7 @@
 class_name EffectStats extends Node
 
-enum Effect { SLOW, POISON }
-
 @export
 var stats_enabled := true
-
-@export
-var type := Effect.SLOW
 
 @export_range(1, 5)
 var fire_rate: int = 1
@@ -14,9 +9,10 @@ var fire_rate: int = 1
 @export_range(1, 4)
 var effect_range: int = 2
 
-func create():
-    match type:
-        Effect.SLOW:
-            return SlowEffect.new()
+# TODO: allow setting effect duration
 
-    return null
+@export
+var effect: PackedScene
+
+func create():
+    return effect.instantiate()
