1a04062dc122a9548053dec50051d798b962c988
Julian L <julian.lawrance@gmail.com>
2024-05-25 15:20:32 +0100

    Create separate node and script for handling ray cast damage

--- scripts/firing_line.gd
+++ scripts/firing_line.gd
@@ -2,6 +2,7 @@ class_name FiringLine extends Node2D
 
 @onready var ray_cast: RayCast2D = $RayCast
 @onready var line: Line2D = $Line
+@onready var ray_cast_damage: RayCastDamage = $RayCastDamage
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 @export var enabled := true
@@ -15,29 +16,7 @@ func set_target(projectile_range: int):
 func can_see_enemies():
 	return ray_cast.is_colliding()
 
-# TODO: move all of this enemy-damaging logic to a child node
 func fire(bolt_stats: TowerLevelStats):
 	animation_player.play("fire")
 
-	var enemy_collider = ray_cast.get_collider()
-
-	if bolt_stats.infinite_penetration:
-		while enemy_collider != null:
-			enemy_collider = process_enemy(enemy_collider, bolt_stats)
-	else:
-		process_enemy(enemy_collider, bolt_stats)
-
-	ray_cast.clear_exceptions()
-
-func process_enemy(enemy_collider: CollisionObject2D, bolt_stats: TowerLevelStats):
-	damage_enemy(enemy_collider, bolt_stats)
-	return next_enemy()
-
-func damage_enemy(enemy_collider: CollisionObject2D, bolt_stats: TowerLevelStats):
-	var enemy := (enemy_collider as CollisionObject2D).get_parent() as Enemy
-	enemy.handle_bolt(bolt_stats)
-	ray_cast.add_exception(enemy_collider)
-
-func next_enemy():
-	ray_cast.force_raycast_update()
-	return ray_cast.get_collider()
+	ray_cast_damage.process_enemies(ray_cast, bolt_stats)
