9e87a15705f68a819612ba568f41b7b753990d71
Julian Lawrance <julian.lawrance@gmail.com>
2024-05-31 21:15:01 +0100

    Implement new path

--- scripts/path_manager.gd
+++ scripts/path_manager.gd
@@ -3,12 +3,15 @@ class_name PathManager extends Node2D
 @export
 var paths: Array[Path] = []
 
+@export_range(0, 1)
+var initial_path_index := 0
+
 signal enemy_died(enemy: Enemy)
 signal enemy_reached_end(enemy: Enemy)
 
 func _ready():
 	for i in range(paths.size()):
-		if i == 0:
+		if i == initial_path_index:
 			paths[i].show()
 			paths[i].enemy_died.connect(_on_path_enemy_died)
 			paths[i].enemy_reached_end.connect(_on_path_enemy_reached_end)
@@ -16,7 +19,7 @@ func _ready():
 			paths[i].hide()
 
 func get_active_path():
-	return paths[0]
+	return paths[initial_path_index]
 
 func spawn_enemy(enemy_scene: PackedScene):
 	# TODO: do the signal-connecting entirely in this script
