64a64b7ba141d6a3e332cba74fbf321db3f6e4a5
Julian Lawrance <julian.lawrance@gmail.com>
2024-05-31 21:26:15 +0100

    Turn path manager into a tool script

--- scripts/path_manager.gd
+++ scripts/path_manager.gd
@@ -1,25 +1,40 @@
+@tool
 class_name PathManager extends Node2D
 
 @export
 var paths: Array[Path] = []
 
-@export_range(0, 1)
-var initial_path_index := 0
+@export
+var active_path_index: int:
+	set(value):
+		if Engine.is_editor_hint():
+			if value < 0 or value > paths.size() - 1:
+				return
+
+		active_path_index = value
+		set_active_path()
 
 signal enemy_died(enemy: Enemy)
 signal enemy_reached_end(enemy: Enemy)
 
 func _ready():
+	set_active_path()
+
+func set_active_path():
+	print("Setting active path " + str(active_path_index))
+
 	for i in range(paths.size()):
-		if i == initial_path_index:
+		if i == active_path_index:
 			paths[i].show()
-			paths[i].enemy_died.connect(_on_path_enemy_died)
-			paths[i].enemy_reached_end.connect(_on_path_enemy_reached_end)
+
+			if not Engine.is_editor_hint():
+				paths[i].enemy_died.connect(_on_path_enemy_died)
+				paths[i].enemy_reached_end.connect(_on_path_enemy_reached_end)
 		else:
 			paths[i].hide()
 
 func get_active_path():
-	return paths[initial_path_index]
+	return paths[active_path_index]
 
 func spawn_enemy(enemy_scene: PackedScene):
 	# TODO: do the signal-connecting entirely in this script
