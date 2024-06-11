d565754099750af15646c9ed5f6e09a5c4ffaa59
Julian Lawrance <julian.lawrance@gmail.com>
2024-06-11 18:08:51 +0100

    Create double cheese upgrade for poison tower

--- scripts/effect_stats.gd
+++ scripts/effect_stats.gd
@@ -15,7 +15,7 @@ var effect_range := 2.0:
         adjust_range.emit(value)
 
 ## The length of time that the effect lasts for, in seconds.
-@export_range(1.0, 5.0)
+@export_range(1.0, 10.0)
 var effect_duration := 1.0
 
 @export
