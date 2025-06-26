5539b2814f24a740d18155f706f31df12ece908b
Julian L <julian.lawrance@gmail.com>
2025-06-26 19:42:50 +0100

    Move tower selection key handling to KeyboardShortcuts

--- scenes/autoloads/keyboard_shortcuts.gd
+++ scenes/autoloads/keyboard_shortcuts.gd
@@ -3,3 +3,9 @@ extends Node
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_text_delete"):
 		TowerEvents.emit_tower_sold()
+
+	if Input.is_action_just_pressed("next_tower"):
+		TowerEvents.emit_next_tower()
+
+	if Input.is_action_just_pressed("previous_tower"):
+		TowerEvents.emit_previous_tower()
--- scenes/ui/game_ui/states/game_ui_state_enabled.gd
+++ scenes/ui/game_ui/states/game_ui_state_enabled.gd
@@ -13,12 +13,6 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		TowerEvents.emit_tower_deselected()
 
-	if Input.is_action_just_pressed("next_tower"):
-		TowerEvents.emit_next_tower()
-
-	if Input.is_action_just_pressed("previous_tower"):
-		TowerEvents.emit_previous_tower()
-
 func _on_tower_created(tower_scene: PackedScene) -> void:
 	transition_state(GameUI.State.CREATING_TOWER, GameUIStateData.build().with_tower_scene(tower_scene))
 
