aecee7b08a4696a472bf71a742f1987fae5fc0ab
Julian L <julian.lawrance@gmail.com>
2025-06-28 19:55:40 +0100

    Create better explosion animation using a shader

--- scenes/projectiles/explosion.gd
+++ scenes/projectiles/explosion.gd
@@ -1,6 +1,35 @@
-class_name Explosion extends Node2D
+@tool
+class_name Explosion
+extends Node2D
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@export_range(0.1, 1.0)
+var time_period := 1.0:
+	set(value):
+		time_period = value
 
-func explode():
-	animation_player.play("explode")
+		if Engine.is_editor_hint():
+			_shader_updater.update_time_period(time_period)
+
+@onready
+var sprite: AnimatedSprite2D = %Sprite
+
+var _shader_updater: ShaderTimeUpdater = null
+var _current_time := 0.0
+
+func _ready() -> void:
+	var explosion_material := sprite.material as ShaderMaterial
+	assert(explosion_material)
+
+	_shader_updater = ShaderTimeUpdater.new(explosion_material)
+	_shader_updater.update_time_period(time_period)
+
+func _process(delta: float) -> void:
+	_current_time += delta
+
+	if _current_time > time_period:
+		_current_time = 0
+
+		if not Engine.is_editor_hint():
+			queue_free()
+
+	_shader_updater.update_current_time(_current_time)
---
+++ scenes/projectiles/shader_time_updater.gd
@@ -0,0 +1,12 @@
+class_name ShaderTimeUpdater
+
+var _material: ShaderMaterial = null
+
+func _init(material: ShaderMaterial) -> void:
+	_material = material
+
+func update_time_period(time_period: float) -> void:
+	_material.set_shader_parameter("time_period", time_period)
+
+func update_current_time(current_time: float) -> void:
+	_material.set_shader_parameter("current_time", current_time)
