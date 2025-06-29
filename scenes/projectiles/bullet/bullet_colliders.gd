6a69077d27afc66f1836e5d34690c5c7c02d1f89
Julian L <julian.lawrance@gmail.com>
2025-06-29 10:20:59 +0100

    Create bullet colliders script

---
+++ scenes/projectiles/bullet/bullet_colliders.gd
@@ -0,0 +1,17 @@
+class_name BulletColliders
+extends Node2D
+
+@export
+var collision_area: ProjectileArea2D
+
+signal enemy_hit(enemy: Enemy)
+
+func _ready() -> void:
+	collision_area.area_entered.connect(_handle_area_entered)
+
+func setup(projectile: Projectile) -> void:
+	collision_area.setup(projectile)
+
+func _handle_area_entered(enemy_body: EnemyArea2D) -> void:
+	var enemy := enemy_body.get_enemy()
+	enemy_hit.emit(enemy)
