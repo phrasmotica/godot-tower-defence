e6b25cc609bab0169a0c3256f80b6dcbca8a830d
Julian L <julian.lawrance@gmail.com>
2025-06-30 08:32:09 +0100

    Convert TowerLevelManager into a regular class. Assign a tower's base level to their weaponry script

--- scenes/towers/tower_level_manager.gd
+++ scenes/towers/tower_level_manager.gd
@@ -1,39 +1,37 @@
-@tool
 class_name TowerLevelManager
-extends Node
 
-@export
-var base_level: TowerLevel
+var _base_level: TowerLevel = null
+var _upgrade_path: Array[int] = []
+var _ongoing_upgrade_index := -1
 
-var upgrade_path: Array[int] = []
-
-var ongoing_upgrade_index := -1
+func _init(base_level: TowerLevel) -> void:
+	_base_level = base_level
 
 func start_upgrade(index: int) -> TowerLevel:
 	var next_level := get_upgrade(index)
 
 	if next_level:
-		ongoing_upgrade_index = index
+		_ongoing_upgrade_index = index
 
 	return next_level
 
 func finish_upgrade() -> TowerLevel:
-	if ongoing_upgrade_index < 0 or not get_upgrade(ongoing_upgrade_index):
+	if _ongoing_upgrade_index < 0 or not get_upgrade(_ongoing_upgrade_index):
 		return
 
-	upgrade_path.append(ongoing_upgrade_index)
-	ongoing_upgrade_index = -1
+	_upgrade_path.append(_ongoing_upgrade_index)
+	_ongoing_upgrade_index = -1
 
 	return get_current_level()
 
 func get_upgrade(index: int) -> TowerLevel:
-	return base_level.get_upgrade(upgrade_path, index)
+	return _base_level.get_upgrade(_upgrade_path, index)
 
 func get_current_level() -> TowerLevel:
-	return base_level.get_current_level(upgrade_path)
+	return _base_level.get_current_level(_upgrade_path)
 
 func get_all_levels() -> Array[TowerLevel]:
-	return base_level.get_all_levels()
+	return _base_level.get_all_levels()
 
 func get_total_value() -> int:
-	return base_level.get_total_value(upgrade_path)
+	return _base_level.get_total_value(_upgrade_path)
