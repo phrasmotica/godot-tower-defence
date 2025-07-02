2459ed8e99c18ec4ae666c7b884a5d0d6d03d1a1
Julian L <julian.lawrance@gmail.com>
2025-07-02 22:33:37 +0100

    Ensure the current placing tower can be swapped for a different one, by clicking a different button or pressing a different button's shortcut key

--- scenes/ui/game_ui/states/game_ui_state_placing_tower.gd
+++ scenes/ui/game_ui/states/game_ui_state_placing_tower.gd
@@ -1,39 +1,44 @@
 class_name GameUIStatePlacingTower
 extends GameUIState
 
-var placing_tower: Tower
+var _placing_tower: Tower
 
 func _enter_tree() -> void:
 	print("Game UI is now placing tower")
 
-	placing_tower = _state_data.get_placing_tower()
-	if not placing_tower:
+	_placing_tower = _state_data.get_placing_tower()
+	if not _placing_tower:
 		print("No tower to place!")
 
 		transition_state(GameUI.State.ENABLED)
 
+	TowerEvents.tower_created.connect(_on_tower_created)
+
 	TowerEvents.tower_placing_cancelled.connect(_on_tower_placing_cancelled)
 	TowerEvents.tower_placing_finished.connect(_on_tower_placing_finished)
 
-	_game_ui.add_child(placing_tower)
+	_game_ui.add_child(_placing_tower)
 
-	TowerEvents.emit_tower_placing_started(placing_tower)
+	TowerEvents.emit_tower_placing_started(_placing_tower)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		TowerEvents.emit_tower_placing_cancelled()
 
+func _on_tower_created(tower_scene: PackedScene) -> void:
+	cancel()
+	transition_state(GameUI.State.CREATING_TOWER, GameUIStateData.build().with_tower_scene(tower_scene))
+
 func _on_tower_placing_cancelled() -> void:
 	cancel()
+	finish()
 
 func _on_tower_placing_finished(_tower: Tower) -> void:
 	finish()
 
 func cancel() -> void:
-	placing_tower.queue_free()
-	placing_tower = null
-
-	finish()
+	_placing_tower.queue_free()
+	_placing_tower = null
 
 func finish() -> void:
 	transition_state(GameUI.State.ENABLED)
