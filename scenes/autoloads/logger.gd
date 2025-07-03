daa24fc6e91d460c3a0d7a9c2be1b768582cad38
Julian L <julian.lawrance@gmail.com>
2025-07-03 08:44:55 +0100

    Wrap around GoLogger autoload inside our Logger autoload

--- scenes/autoloads/logger.gd
+++ scenes/autoloads/logger.gd
@@ -15,15 +15,15 @@ var _prefixes := {
 
 func _init() -> void:
 	if not Engine.is_editor_hint():
-		_print_message("Minimum logging level: %s" % _prefixes[MIN_LEVEL], Level.INFO)
+		_print_message("Minimum console logging level: %s" % _prefixes[MIN_LEVEL], Level.INFO, -1)
 
 func fatal(msg: String) -> void:
 	if Level.FATAL <= MIN_LEVEL:
-		_print_message(msg, Level.FATAL)
+		_print_message(msg, Level.FATAL, 1)
 
 func error(msg: String) -> void:
 	if Level.ERROR <= MIN_LEVEL:
-		_print_message(msg, Level.ERROR)
+		_print_message(msg, Level.ERROR, 1)
 
 func warning(msg: String) -> void:
 	if Level.WARNING <= MIN_LEVEL:
@@ -37,5 +37,9 @@ func debug(msg: String) -> void:
 	if Level.DEBUG <= MIN_LEVEL:
 		_print_message(msg, Level.DEBUG)
 
-func _print_message(msg: String, level: Level) -> void:
-	print("[%s] %s" % [_prefixes[level], msg])
+func _print_message(msg: String, level: Level, category := 0) -> void:
+	var resolved_message := "[%s] %s" % [_prefixes[level], msg]
+	print(resolved_message)
+
+	if category >= 0:
+		Log.entry(resolved_message, category)
