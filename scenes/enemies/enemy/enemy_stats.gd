0925db9ab84ad70446d98dcb0171e9d5ab661f86
Julian L <julian.lawrance@gmail.com>
2025-07-05 23:51:36 +0100

    Add TODO item

--- scenes/enemies/enemy/enemy_stats.gd
+++ scenes/enemies/enemy/enemy_stats.gd
@@ -3,6 +3,7 @@ class_name EnemyStats extends Node
 @export_range(1.0, 20.0)
 var starting_health := 5.0
 
+# BUG: this is accessed as "current_health" in a few places. Fix those
 var _current_health: float
 
 func _ready() -> void:
