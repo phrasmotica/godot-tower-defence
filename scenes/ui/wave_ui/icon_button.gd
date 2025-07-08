1a3ec62aad0bc07f76e19429d2a7b75f6c903aa4
Julian Lawrance <julian.lawrance@gmail.com>
2025-07-08 23:27:39 +0100

    Create scene for a button that contains a centred icon. Allow picking the icon colour and size

---
+++ scenes/ui/wave_ui/icon_button.gd
@@ -0,0 +1,44 @@
+@tool
+class_name IconButton
+extends Button
+
+@export
+var texture: Texture2D:
+	set(value):
+		texture = value
+
+		_refresh()
+
+@export
+var texture_size := 32.0 * Vector2.ONE:
+	set(value):
+		texture_size = value
+
+		_refresh()
+
+@export
+var texture_colour := Color.WHITE:
+	set(value):
+		texture_colour = value
+
+		_refresh()
+
+@onready
+var texture_rect: TextureRect = %TextureRect
+
+var _updater: ShaderUpdater = null
+
+func _ready() -> void:
+	var texture_material := texture_rect.material as ShaderMaterial
+	assert(texture_material)
+
+	_updater = ShaderUpdater.new(texture_material)
+
+	_refresh()
+
+func _refresh() -> void:
+	if texture_rect:
+		texture_rect.custom_minimum_size = texture_size
+		texture_rect.texture = texture
+
+	_updater.set_color("replacement_colour", texture_colour)
